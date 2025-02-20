53f2ebf9d7eb2e96cbaffd2c5b53a1d6de48f7b6
LewBlu <lewis.blundell.development@gmail.com>
2025-02-20 21:06:50 +0000

    Changed articles to response to livewire update

--- laravel/app/Livewire/Article/All.php
+++ laravel/app/Livewire/Article/All.php
@@ -3,18 +3,40 @@
 namespace App\Livewire\Article;
 
 use App\Models\Article;
+
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class All extends Component
 {
-
     use WithPagination;
+
+    public ?string $selectedTag = null;
 
     public function render()
     {
         return view('livewire.article.all')->with([
-            'articles' => Article::with('tags')->paginate(10)
+            'articles' => $this->getArticles()
         ]);
+    }
+
+    #[On('article-filter')]
+    public function updateSelectedTag(?string $tag)
+    {
+        $this->selectedTag = $this->selectedTag == $tag ? null : $tag;
+    }
+
+    public function getArticles()
+    {
+        return Article::query()
+            ->when(!is_null($this->selectedTag), function(Builder $query){
+                return $query->whereHas('tags',function (Builder $query) {
+                    return $query->where('name', $this->selectedTag);
+                });
+            })
+            ->with('tags')
+            ->get();
     }
 }
--- laravel/resources/views/livewire/tag/all.blade.php
+++ laravel/resources/views/livewire/tag/all.blade.php
@@ -1,5 +1,10 @@
 <div class="flex flex-wrap gap-2">
     @foreach($tags as $tag)
-    <div class="bg-gray-300 text-gray-800 text-sm px-3 rounded-full dark:bg-gray-700 dark:text-gray-300">{{ $tag->name }}</div>
+    <div
+        class="bg-gray-300 text-gray-800 text-sm px-3 rounded-full dark:bg-gray-700 dark:text-gray-300"
+        wire:click="$dispatch('article-filter', { tag: '{{ $tag->name }}' })"
+    >
+        {{ $tag->name }}
+    </div>
     @endforeach
 </div>
