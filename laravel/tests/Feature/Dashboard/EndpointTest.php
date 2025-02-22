c0f69441e97e0cfcc22e7af14ec3fae751cf8693
LewBlu <lewis.blundell.development@gmail.com>
2025-02-22 09:10:38 +0000

    Updated endpoint tests

--- laravel/tests/Feature/Dashboard/EndpointTest.php
+++ laravel/tests/Feature/Dashboard/EndpointTest.php
@@ -9,13 +9,16 @@ describe('dashboard endpoints', function(){
         });
 
         test('index', function () {
-            $this->actingAs($this->user)->get(route('dashboard.index'))->assertStatus(200);
+            $this->actingAs($this->user)
+                ->get(route('dashboard.index'))
+                ->assertOk();
         });
     });
 
     describe('without permission', function() {
         test('index', function () {
-            $this->get(route('dashboard.index'))->assertRedirect();
+            $this->get(route('dashboard.index'))
+                ->assertRedirect(route('login'));
         });
     });
 });
