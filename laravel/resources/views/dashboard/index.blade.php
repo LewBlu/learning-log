506514cffc50ccf566adcec0a02aa099ff16b404
LewBlu <lewis.blundell.development@gmail.com>
2025-02-23 10:36:48 +0000

    Changed tags all to standard component

--- laravel/resources/views/dashboard/index.blade.php
+++ laravel/resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="my-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <livewire:tag.all />
+        <x-tag.all />
     </div>
     <livewire:article.all />
 </x-app-layout>
